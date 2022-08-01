f5703abc398ed832888c304454977820dfd07c27
Jason Ding <zhoujie.ding@berkeley.edu>
2022-08-01 02:08:02 -0400

    add label and linked commits

--- collect_issues.py
+++ collect_issues.py
@@ -21,24 +21,48 @@ def collect_issue(gh_pat, repo_name):
     for issue in total_issues:
         try:
             cnt += 1
-            issue_data = {
-                "title": issue.title,
-                "body": issue.body,
-                "author_name": issue.user.name,
-                "created_at": issue.created_at.strftime("%a, %d %b %Y %H:%M:%S %z").strip(),
-                "closed_at": issue.closed_at.strftime("%a, %d %b %Y %H:%M:%S %z").strip(),
-                "closed_by": issue.closed_by.name,
-                "last_modified": issue.last_modified,
-                "comments": []
-            }
-            for comment in issue.get_comments():
-                issue_data["comments"].append({
-                    "body": comment.body,
-                    "created_at": comment.created_at.strftime("%a, %d %b %Y %H:%M:%S %z").strip(),
-                    "created_by": comment.user.name,
-                    "last_modified": comment.last_modified
-                })
+            try:
+                issue_data = {
+                    "title": issue.title,
+                    "body": issue.body,
+                    "author_name": issue.user.name,
+                    "created_at": issue.created_at.strftime("%a, %d %b %Y %H:%M:%S %z").strip(),
+                    "closed_at": issue.closed_at.strftime("%a, %d %b %Y %H:%M:%S %z").strip(),
+                    "closed_by": issue.closed_by.name,
+                    "last_modified": issue.last_modified,
+                    "comments": [],
+                    "linked_commits": [],
+                    "labels": []
+                }
+                for comment in issue.get_comments():
+                    issue_data["comments"].append({
+                        "body": comment.body,
+                        "created_at": comment.created_at.strftime("%a, %d %b %Y %H:%M:%S %z").strip(),
+                        "created_by": comment.user.name,
+                        "last_modified": comment.last_modified
+                    })
+            except AttributeError:
+                continue
+
+            for event in issue.get_events():
+                try:
+                    if event.commit_id:
+                        issue_data["linked_commits"].append({
+                            "commit_id": event.commit_id,
+                            "event": event.event
+                        })
+                except AttributeError:
+                    continue
+            for label in issue.get_labels():
+                try:
+                    issue_data["labels"].append({
+                        "name": label.name,
+                        "description": label.description
+                    })
+                except AttributeError:
+                    continue
             issue_list.append(issue_data)
+            
             if cnt % 50 == 0:
                 print("Already collected:", cnt)
             if cnt % 10000 == 0:
@@ -49,14 +73,19 @@ def collect_issue(gh_pat, repo_name):
                 with open("github_issue_corpus_names.txt", "a") as f:
                     f.write(repo_name + "_issue_corpus_" + str(num) + ".txt\n")
                 issue_list = []
-        except AttributeError:
-            continue
+
         except RateLimitExceededException:
             print("Number of issues collected before encountering error:", cnt)
             limits = gh.get_rate_limit()
-            reset = limits.core.reset.replace(tzinfo=timezone.utc)
+            if limits.search.remaining == 0:
+                limited = limits.search
+            elif limits.graphql.remaining == 0:
+                limited = limits.graphql
+            else:
+                limited = limits.core
+            reset = limited.reset.replace(tzinfo=timezone.utc)
             now = datetime.now(timezone.utc)
-            seconds = (reset - now).total_seconds() + 5
+            seconds = (reset - now).total_seconds() + 60
             print("Rate limit exceeded.")
             print(f"Reset is in {seconds:.3g} seconds.")
             if seconds > 0.0:
