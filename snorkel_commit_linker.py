b35f315bc55e85433363731f4f9aada9acbfd433
Jason Ding <zhoujie.ding@berkeley.edu>
2022-09-07 03:12:58 -0400

    update

--- snorkel_commit_linker.py
+++ snorkel_commit_linker.py
@@ -1,6 +1,7 @@
 import os
 import json
 import pickle
+from tkinter import E
 from issue_linker import *
 from entities import EntityEncoder, GithubIssue
 
@@ -23,11 +24,11 @@ def extract_issue_text_terms_parts(issue, limit_feature):
 
     return terms_parts
 
-def load_github_issues():
+def load_github_issues(file_names):
     print("Start loading crawled github issues...")
     github_issues = []
 
-    file_names = utils.read_lines(os.path.join(directory, 'github_issue_corpus_names.txt'))
+    # file_names = utils.read_lines(os.path.join(directory, 'github_issue_corpus_names.txt'))
     id_count = -1
     file_names = ["php-php-src_issue_corpus.txt"]
     for file_name in file_names:
@@ -43,12 +44,12 @@ def load_github_issues():
     print("Finished loading crawled github issues")
     return github_issues
 
-def calculate_similarity_scores(records, github_issues, tfidf_vectorizer, using_code_terms_only):
-    # TODO: save and load record corpus instead as it will get very big
+def prepare_for_calculation(records, using_code_terms_only=False):
+    print("Preparing files for calculating similarity.")
     record_codes = []
     record_texts = []
     record_index = -1
-    record_code_id = []
+    record_code_id = [-1]
     for record in records:
         record_codes.append(record.code_terms)
         if not using_code_terms_only:
@@ -56,12 +57,25 @@ def calculate_similarity_scores(records, github_issues, tfidf_vectorizer, using_
                 record_index += 1
                 record_texts.append(text_terms)
             record_code_id.append(record_index)
-            # add an index counter here to get record index
-            # have another array to store record text terms
     record_code_id = np.array(record_code_id)
 
-    # print("Calculating TF-IDF vectorizer...")
-    # print("record matrix shape is:", record_matrix.shape)
+    corpus = record_codes + record_texts
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_vectorizer = tfidf_vectorizer.fit(corpus)
+    record_code_matrix = tfidf_vectorizer.transform(record_codes)
+    record_text_matrix = tfidf_vectorizer.transform(record_texts)
+    with open("commit_linker_data/record_code_ind.npy", "wb+") as f:
+        np.save(f, np.array(record_code_id))
+    with open("commit_linker_data/tfidf_vectorizer.joblib", "wb+") as f:
+        dump(tfidf_vectorizer, f)
+    with open("commit_linker_data/record_code_mat.npz", "wb+") as f:
+        save_npz(f, record_code_matrix)
+    with open("commit_linker_data/record_text_mat.npz", "wb+") as f:
+        save_npz(f, record_text_matrix)
+    print("Finished.")
+
+def calculate_similarity_scores(records, github_issues, record_code_id,\
+    record_code_matrix, record_text_matrix, tfidf_vectorizer, using_code_terms_only=False):
     
     issue_corpus = []
     for issue in github_issues:
@@ -69,13 +83,7 @@ def calculate_similarity_scores(records, github_issues, tfidf_vectorizer, using_
         if not using_code_terms_only:
             for text_terms in issue.text_terms_parts:
                 issue_corpus.append(text_terms)
-    corpus = record_codes + record_texts + issue_corpus
-    tfidf_vectorizer = tfidf_vectorizer.fit(corpus)
-    record_code_matrix = tfidf_vectorizer.transform(record_codes)
-    record_text_matrix = tfidf_vectorizer.transform(record_texts)
     issue_matrix = tfidf_vectorizer.transform(issue_corpus)
-    # print("issue matrix shape is:", issue_matrix.shape)
-    # print("Finish calculating TF-IDF vectorizer")
 
     score_lines = []
     issue_count = 0
@@ -98,7 +106,7 @@ def calculate_similarity_scores(records, github_issues, tfidf_vectorizer, using_
                 best_record = records[max_ind]
             else:
                 max_ind = csr_matrix.argmax(score_matrix_text) % record_text_matrix.shape[0]
-                record_ind = np.where(record_code_id <= max_ind)[0][-1]
+                record_ind = np.where(record_code_id < max_ind)[0][-1]
                 best_record = records[record_ind]
 
         if best_record is not None:
@@ -113,47 +121,6 @@ def calculate_similarity_scores(records, github_issues, tfidf_vectorizer, using_
         if issue_count % 50 == 0:
             print("Finished {} issues".format(issue_count))
     # TODO: the problem with issues is that it might take long time to transform
-    return score_lines
-
-# input: record -- single record with no issue linked
-# records -- [record] used in for loop
-def process_linking(issues, records, testing=False, min_df=1, using_code_terms_only=False, limit_feature=False, 
-                    text_feature_min_length=0, output_file_name='texts.txt', chunk=-1, 
-                    relevant_ticket=False, test_true_link=False, merge_link=False, max_df=1):
-    global terms_min_length
-    terms_min_length = text_feature_min_length
-
-    global similarity_scores_file_path
-    similarity_scores_file_path = os.path.join(directory, output_file_name)
-
-    global chunk_size
-    chunk_size = chunk
-
-    global use_relevant_ticket
-    use_relevant_ticket = relevant_ticket
-
-    # print("length of the commits is:", len(records))
-    # for record in records:
-    #     record.code_terms = extract_commit_code_terms(record)
-    #     if not using_code_terms_only:
-    #         record.text_terms_parts = extract_commit_text_terms_parts(record)
-
-    # github_issues = load_github_issues()
-    # print("Issues length: {}".format(len(github_issues)))
-    # print("Start extracting issue features...")
-    for issue in issues:
-        issue.code_terms = extract_issue_code_terms(issue)
-        if not using_code_terms_only:
-            issue.text_terms_parts = extract_issue_text_terms_parts(issue, limit_feature)
-    # print("Finish extracting issue features")
-    
-    tfidf_vectorizer = TfidfVectorizer()
-    if min_df != 1:
-        tfidf_vectorizer.min_df = min_df
-    if max_df != 1:
-        tfidf_vectorizer.max_df = max_df
-
-    score_lines = calculate_similarity_scores(records, issues, tfidf_vectorizer, using_code_terms_only)
     return score_lines
 
 # sim_scores_file = score_lines
@@ -199,27 +166,59 @@ def write_dataset_with_enhanced_issue(issues, records, score_lines, thresh=0.5, 
     return issues
 
 def main():
-    records = data_loader.load_records(os.path.join(directory, 'prediction/php/full_dataset_with_all_features.txt'))
+    records = data_loader.load_records(os.path.join(directory,
+    '/space2/ding/dl-vulnerability-detection/scripts/snorkel/repo_data/php-full_dataset_with_all_features-limited.txt'))
+    print("length of the records is:", len(records))
     for record in records:
         record.code_terms = extract_commit_code_terms(record)
         record.text_terms_parts = extract_commit_text_terms_parts(record)
-    issues = load_github_issues()
+    # If first time running, please run the following function.
+    # Otherwise, comment it out.
+    prepare_for_calculation(records)
+    
+    issues = load_github_issues(["php-php-src_issue_corpus.txt"])
     print("length of the issues is:", len(issues))
     issue_linked = []
     cnt = 0
+    
+    print("Start loading files from commit_linker_data/")
+    with open("commit_linker_data/record_code_ind.npy", "rb") as f:
+        record_code_id = np.load(f, allow_pickle=True)
+    with open("commit_linker_data/record_code_mat.npz", "rb") as f:
+        record_code_matrix = load_npz(f)
+    with open("commit_linker_data/record_text_mat.npz", "rb") as f:
+        record_text_matrix = load_npz(f)
+    with open("commit_linker_data/tfidf_vectorizer.joblib", "rb") as f:
+        tfidf_vectorizer = load(f)
+    print("Finish loading files from commit_linker_data/")
+    in_the_list = 0
     for issue in issues:
-        if len(issue.linked_commits) == 0:
-            cnt += 1
-            score_lines = process_linking([issue], records)
+        if len(issue.linked_commits) != 0:
+            issue.code_terms = extract_issue_code_terms(issue)
+            issue.text_terms_parts = extract_issue_text_terms_parts(issue, False)
+            score_lines = calculate_similarity_scores(records, [issue], record_code_id,\
+                record_code_matrix, record_text_matrix, tfidf_vectorizer)
             print(score_lines[0])
             issue = write_dataset_with_enhanced_issue([issue], records, score_lines)[0]
             issue.__dict__.pop("code_terms")
             issue.__dict__.pop("text_terms_parts")
+            commit_id_lst = []
+            linked_one = ""
+            for c in issue.linked_commits:
+                if c["event"] == "link_recovery":
+                    linked_one = c["commit_id"]
+                else:
+                    commit_id_lst.append(c["commit_id"])
+            if linked_one != "":
+                cnt += 1
+            if linked_one in commit_id_lst:
+                in_the_list += 1
+                print("found one")
         issue.__dict__.pop("id")
         issue_linked.append(issue)
-        if cnt == 30:
-            break
-
+    
+    print("The number of issues with linked commits is:", str(cnt))
+    print("The number of correct linkage is:", str(in_the_list))
     entity_encoder = EntityEncoder()
     issues_with_record = entity_encoder.encode(issue_linked)
     with open(os.path.join(directory, "enhanced_issue_corpus/php.txt"), "w+") as f:
